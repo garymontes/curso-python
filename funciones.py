567c9e7a3cd18b0c5ce29374b8975b1b64104457
Gary Montes <gary.montes83@gmail.com>
2024-09-13 09:06:56 -0500

    --- Creación archivo funciones.py

---
+++ funciones.py
@@ -0,0 +1,5 @@
+def saludo ():
+    print('Había una vez una viejecita que vivía en una casita muy humilde.')
+    print('A pesar de su pobreza, siempre estaba feliz y agradecida por lo poco que tenía')
+
+saludo()
