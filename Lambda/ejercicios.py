9813accbdd1af94bf213266a7fd40aa224015c56
Gary Montes <gary.montes83@gmail.com>
2024-10-11 17:08:05 -0500

    --- Creando carpeta lamba y practicando

---
+++ Lambda/ejercicios.py
@@ -0,0 +1,16 @@
+# Ejercicios varios utilizando lambda 
+print()
+def suma():
+    
+    suma= lambda a,x : a+x
+    print(suma(4,5))
+
+def resta():
+    
+    resta= lambda b,c : b-c
+    print(f'La resta de la función Lamba es: {resta(10,8)}')
+
+    print()
+if __name__=='__main__':
+    resta()
+
