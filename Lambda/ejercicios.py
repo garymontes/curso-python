d102587d82da105126853c28a9c2a4405ef3e107
Gary Montes <garymontes>
2024-10-15 07:39:38 -0500

    --- Practicando funciones lambda

--- Lambda/ejercicios.py
+++ Lambda/ejercicios.py
@@ -9,8 +9,12 @@ def resta():
     
     resta= lambda b,c : b-c
     print(f'La resta de la función Lamba es: {resta(10,8)}')
+    
+def multiplicacion():
+    mult= lambda a,x : a*x
+    print(f'La multiplicación es: {mult(5,5)}')
 
     print()
 if __name__=='__main__':
-    resta()
+    multiplicacion()
 
