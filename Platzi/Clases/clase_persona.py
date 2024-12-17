40ca3425518bec971e36e3ede2c8ebfd6de0e94b
garymontes <gary.montes83@gmail.com>
2024-12-16 22:00:52 -0500

    --- Practicando clases

--- Platzi/Clases/clase_persona.py
+++ Platzi/Clases/clase_persona.py
@@ -1,7 +1,5 @@
 class Persona:
     
-    '''programa crea una clase con los datos pesonales de una persona'''
-    
     def __init__(self,nombre, edad, sexo, correo,pais, ciudad, empleo):
         self.nombre=nombre
         self.edad=edad
@@ -14,16 +12,34 @@ class Persona:
     def descripcion(self):
         return (f'{self.nombre} tiene: {self.edad} años, sexo: {self.sexo}, correo: {self.correo}, pais: {self.pais}, vive en: {self.ciudad} y su profesión es: {self.empleo}')
 
+class Gente:
+    def __init__(self, nombre, apellido, edad):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.edad=edad
+
+    def g(self):
+        print(f'El nombre es: {self.nombre} {self.apellido} y su edad es: {self.edad} años')
+
 
 def run():
-    persona1=Persona('Gary Montes',41,'masculino','gary.montes@hotmail.com','colombia','cartagena','ingeniero')
-    print(persona1.descripcion())
+        persona1=Persona('Gary Montes',41,'masculino','gary.montes@hotmail.com','colombia','cartagena','ingeniero')
+        print(persona1.descripcion())
 
-    persona2=Persona('Susy Montes',10,'femenino','susy.montes@hotmail.com','colombia','cartagena','estudiante')
-    print(persona2.descripcion())
+        persona2=Persona('Susy Montes',10,'femenino','susy.montes@hotmail.com','colombia','cartagena','estudiante')
+        print(persona2.descripcion())
 
-    
+def llamado():
+    per1=Gente('Gary', 'Montes', 41)  
+    per2=Gente('Susy', 'Montes', 9)
+    per1.g()  
+    per2.g()
 
 
 if __name__=='__main__':
+    llamado()
+    print('*'*50)
     run()
+    
+    
+    print('-'*30)
---
+++ Platzi/Clases/reto_libro.py
@@ -0,0 +1,33 @@
+# Crea una clase Libro que tenga atributos para el título, el autor y el número de páginas. 
+# Implementa un método para imprimir una descripción del libro y otro método para saber si el libro tiene más de 500 páginas.
+
+class Libro:
+    def __init__(self, titulo, autor, paginas):
+        self.titulo=titulo
+        self.autor=autor
+        self.paginas=paginas
+
+    def descripcion(self):
+        print(f'El libro {self.titulo} del autor {self.autor} tiene {self.paginas}')
+
+    
+def run():
+    libro1=Libro('100 años De Soledad', 'Gabriel García Marquez', 520)
+    libro1.descripcion()
+    
+    if libro1.paginas>400:
+        print('Es un libro extenso para leer')
+    else:
+        print('Es un libro fácil de lerr')
+
+    libro2=Libro('La Iliada', 'Homero', 250)
+    libro2.descripcion()
+
+    if libro2.paginas>400:
+        print('Es un libro extenso para leer')
+    else:
+        print('Es un libro fácil de leer')
+    
+
+if __name__=='__main__':
+    run()
