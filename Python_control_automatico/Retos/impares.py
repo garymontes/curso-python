5f2a69ea4f6af875a81e275faf7581e673f3614b
garymontes <gary.montes83@gmail.com>
2026-01-05 16:40:53 -0500

    --- agregando un archivo llamado impar.py a la rama de control automático

---
+++ Python_control_automatico/Retos/impares.py
@@ -0,0 +1,11 @@
+'''
+- Par o impar
+- Solicita un número entero y muestra si es par o impar.
+'''
+print(' ')
+n=int(input('Ingrese un número: '))
+
+if n % 2 == 0:
+    print(f'El número {n} es: par')
+else:
+    print(f'El número {n} es: impar')
